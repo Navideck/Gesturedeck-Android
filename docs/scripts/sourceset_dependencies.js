3701e6a302598a7cc6efba8c59b58214968ceef5
Rohit Sangwan <rohitsangwan647@gmail.com>
2023-07-19 12:16:30 +0530

    add docs

---
+++ docs/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":gesturedeck-android:dokkaHtml/androidTestRelease":[],":gesturedeck-android:dokkaHtml/debug":[],":gesturedeck-android:dokkaHtml/main":[],":gesturedeck-android:dokkaHtml/release":[],":gesturedeck-android:dokkaHtml/testFixtures":[],":gesturedeck-android:dokkaHtml/testFixturesDebug":[],":gesturedeck-android:dokkaHtml/testFixturesRelease":[]}'
