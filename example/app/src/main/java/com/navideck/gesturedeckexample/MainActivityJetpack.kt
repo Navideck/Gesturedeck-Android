4f10160e2da8fe53730bbf24ca6914ab90fff87a
Rohit Sangwan <rohitsangwan647@gmail.com>
2023-10-06 21:23:34 +0530

    Add example apps (#1)
    Add example apps

---
+++ example/app/src/main/java/com/navideck/gesturedeckexample/MainActivityJetpack.kt
@@ -0,0 +1,135 @@
+package com.navideck.gesturedeckexample
+
+import android.content.Context
+import android.os.Bundle
+import android.view.MotionEvent
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import com.navideck.gesturedeck_android.GesturedeckMedia
+import com.navideck.gesturedeck_android.GesturedeckMediaOverlay
+
+class MainActivityJetpack : AppCompatActivity() {
+    private lateinit var gesturedeck: GesturedeckMedia
+    private val isGesturedeckStarted = mutableStateOf(true)
+    private val gesturedeckEvent = mutableStateOf("")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.title = "Gesturedeck Example Jetpack"
+
+        gesturedeck = GesturedeckMedia(
+            this,
+            tapAction = {
+                gesturedeckEvent.value = "Tap"
+            },
+            swipeLeftAction = {
+                gesturedeckEvent.value = "Swipe Left"
+            },
+            swipeRightAction = {
+                gesturedeckEvent.value = "Swipe Right"
+            },
+            panAction = { _, _, _ ->
+                gesturedeckEvent.value = "Pan"
+            },
+            gesturedeckMediaOverlay = GesturedeckMediaOverlay(
+                this,
+                tintColor = ContextCompat.getColor(applicationContext, R.color.primary),
+            ),
+        )
+
+        setContent {
+            homeApp(this)
+        }
+    }
+
+    @Composable
+    private fun homeApp(context: Context) {
+        val systemColorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()
+        MaterialTheme(
+            colorScheme = systemColorScheme.copy(
+                primary = Color(ContextCompat.getColor(context, R.color.primary))
+            ),
+        ) {
+            Surface {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        horizontalArrangement = Arrangement.SpaceAround,
+                    ) {
+                        Button(
+                            enabled = !isGesturedeckStarted.value,
+                            onClick = {
+                                gesturedeck.start()
+                                isGesturedeckStarted.value = true
+                            }) {
+                            Text(text = "Start")
+                        }
+                        Button(
+                            enabled = isGesturedeckStarted.value,
+                            onClick = {
+                                gesturedeck.stop()
+                                isGesturedeckStarted.value = false
+                            }) {
+                            Text(text = "Stop")
+                        }
+                    }
+                    Divider()
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "Gesturedeck Event: ${gesturedeckEvent.value}",
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.W400
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        ev?.let { gesturedeck.onTouchEvent(it) }
+        return super.dispatchTouchEvent(ev)
+    }
+
+    @Composable
+    @Preview(showBackground = true)
+    fun previewHomeApp() {
+        homeApp(LocalContext.current)
+    }
+}
